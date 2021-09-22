1bcd72e4c7407266101398ef651cd64e509db13e
Marc Greyling <marc@cubi201.marcjan.de>
2021-09-22 22:43:46 +0200

    refactoring

--- src/SvgWorld.js
+++ src/SvgWorld.js
@@ -7,7 +7,6 @@ import React, {
 import {
     easeInOutSine,
     heatMapColorforValue,
-    abbrevCountryName,
     loadDemocIdxJson,
     loadCountriesSVGDataJson,
 } from './functions'
@@ -16,7 +15,6 @@ import {
     CRIMSON,
     CRIMSON_HALF,
     GREY,
-    GREY_HALF,
     mainTitle, progressiveLoading,
     stroke,
     strokeWidth,
@@ -25,7 +23,8 @@ import {
 } from './constants'
 import Heatmap from './Heatmap'
 import Loading from './Loading'
-import CountryProfile from "./CountryProfile";
+import CountryListing from './CountryListing'
+import CountryProfile from './CountryProfile'
 
 class SvgWorld extends Component {
 
@@ -532,98 +531,6 @@ const CountryRect = props => {
             strokeWidth={sw}
             fill={'transparent'}
         />
-    )
-}
-
-const CountryListing = ({countries, selected, colour, countryRefs,
-                        handleZoomChange, handleZoomChangeUpateState,
-                        baseSpacing=.25,
-                        fontSize=1,
-                        title='Country list'
-                        }) => {
-
-    if (countries===null) return null
-    // console.log(576, {selected, colour})
-
-    return (
-        <div>
-            <div
-                style={{
-                    fontSize: `${fontSize*1.1}rem`,
-                    fontWeight: 'bolder',
-                    padding: `${baseSpacing*2}rem`,
-                }}
-            >
-                {title}
-            </div>
-            <div
-                style={{ display: 'flex', flexWrap: 'wrap', }}
-            >
-            {countries
-                .filter(e => e.n) /* filter out elements without n (=name) */
-                .sort((a, b) => a.n < b.n ? -1 : 1)
-                .map((country, i) => {
-                    let { n: countryName, id } = country
-                    if (!countryName) return false
-                    countryName = abbrevCountryName(countryName)
-                    countryName = countryName.replace(/\s+/g, '\u00a0') /* nbsp */
-                    countryName = countryName.replace(/-/g, '\u2011') /* hyphen, non-breaking */
-                    // const bgCol = (i % 2 === 0) ? 'transparent' : GREY
-                    const bgCol = (i % 2 === 0) ? 'transparent' : GREY_HALF
-                    const borderAndPadding = (id === selected)
-                        ? {
-                            border: `${baseSpacing}rem solid ${colour}`,
-                            padding: `0 ${baseSpacing*2}rem`,
-                        }
-                        : {
-                            // padding: `${baseSpacing}rem ${baseSpacing*2}rem`,
-                            // border: `${baseSpacing}rem solid ${GREY}`,
-                            border: `${baseSpacing}rem solid transparent`,
-                            padding: `0 ${baseSpacing*2}rem`,
-                        }
-                    return (
-                        <div
-                            key={country.id}
-                            title={country.n}
-                            style={{
-                                ...{
-                                    // flex: 1,
-                                    background: bgCol,
-                                    cursor: 'pointer',
-                                    fontSize: `${fontSize}rem`,
-                                },
-                                ...borderAndPadding
-                            }}
-                            onClick={() => {
-                                // console.log(country.paths)
-                                if ('id' in country && country.id in countryRefs) {
-                                    // console.log(623, countryRefs[country.id])
-                                    const {current} = countryRefs[country.id] || {}
-                                    // console.log(625, current)
-                                    if (current) {
-                                        const {x, y, width, height} = current.getBBox()
-                                        // console.log(628, {x, y, width, height})
-                                        const boundingBox = {
-                                            x,
-                                            y,
-                                            width,
-                                            height,
-                                            sw: 0.3,
-                                        }
-                                        handleZoomChange(50, boundingBox)
-                                    }
-                                }
-                                handleZoomChangeUpateState(country.id)
-                            }}
-                        >
-                            {countryName}
-                        </div>
-                    )
-                })
-                .filter(Boolean)
-            }
-            </div>
-        </div>
     )
 }
 
